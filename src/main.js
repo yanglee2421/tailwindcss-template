3fc494a57ce695361d9c8fe4a7dd93df268b7004
圣 <xtcff082421@gmail.com>
2022-03-25 13:42:31 +0800

    简化仓库

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: 张远航
+ * @Date: 2022-03-21 13:48:41
+ * @LastEditTime: 2022-03-22 18:34:41
+ * @LastEditors: 张圣
+ * @Description: 
+ * @FilePath: \vue3note\src\main.js
+ */
 // 从npm中导入vue对象的createApp方法
 import {
     createApp
