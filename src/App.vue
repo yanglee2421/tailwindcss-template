d0dd7c524f53463f73bc61ee15a2a134de4e4745
张圣 <zhangsheng@fineyun.cn>
2022-05-13 16:18:56 +0800

    inject拿到的是拷贝，props拿到的是get的返回值

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,6 @@
 <template>
-  <div class="b m-1">
-    <p>爷爷</p>
-    <h1 class="m-1">{{ mes }}</h1>
-    <el-button @click="fun">爷爷改信息（脚本）</el-button>
-    <el-button @click="mes = '爷爷改了'">爷爷改信息（模板）</el-button>
-  </div>
-  <div>
-    <Test></Test>
-  </div>
 </template>
 <script setup>
-import Test from "@/components/test.vue"
-import { provide, reactive, ref, watch } from 'vue';
-
-const mes = ref("爷爷给孙子的信息")
-provide('mes', mes)
-const fun = () => {
-  mes.value = "爷爷改了"
-}
-watch(() => mes.name, (newVal, oldVal) => {
-  console.log(newVal);
-  // console.log(oldVal);
-})
 </script>
 <style lang='scss' scoped>
 //
