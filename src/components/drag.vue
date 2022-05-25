ec0a0516118de62360cb1ea0b54ad7e9f82c2f36
张圣 <zhangsheng@fineyun.cn>
2022-05-25 16:34:42 +0800

    拖拽功能小修

--- src/components/drag.vue
+++ src/components/drag.vue
@@ -3,40 +3,41 @@
     <div class="flex-row">
       <el-checkbox-group v-model="value" class="b">
         <div
-          v-for="(item, index) in arr"
+          v-for="(item, index) in arr01"
           :key="item"
           draggable="true"
-          @dragstart="start(index, $event)"
-          @dragover.prevent="over($event)"
-          @dragleave="leave($event)"
-          @drop="drop(index, $event)"
+          @dragstart="dragFn.start(index, $event)"
+          @dragover.prevent="dragFn.over($event)"
+          @dragleave="dragFn.leave($event)"
+          @drop="dragFn.drop(index, $event)"
         >
           <el-checkbox :label="item" />
         </div>
       </el-checkbox-group>
       <div class="b"><el-button @click="log(arr)">输出</el-button></div>
     </div>
-    <progress class="p-1" max="100" value="70">50%</progress>
-    <el-progress :percentage="70"></el-progress>
   </div>
 </template>
 <script setup>
-import { reactive, ref, watchEffect } from "vue";
-const arr = reactive(["123", "456", "789"]);
+import { reactive, ref } from "vue";
 const value = ref([]);
-const start = (index, $event) => {
-  $event.dataTransfer.setData("dragIndex", index);
-};
-const over = ($event) => {
-  $event.currentTarget.style.borderTop = "1px red solid";
-};
-const leave = ($event) => {
-  $event.currentTarget.style.borderTop = "";
-};
-const drop = (index, $event) => {
-  $event.currentTarget.style.borderTop = "";
-  const dragIndex = +$event.dataTransfer.getData("dragIndex");
-  arr.splice(index, 1, ...arr.splice(dragIndex, 1, arr[index]));
+const arr01 = reactive(["123", "456", "789"]);
+const dragFn = {
+  start(index, $event) {
+    $event.dataTransfer.setData("dragIndex", index);
+  },
+  over($event) {
+    $event.currentTarget.style.borderTop = "1px red solid";
+  },
+  leave($event) {
+    $event.currentTarget.style.borderTop = "";
+  },
+  drop(index, $event) {
+    const arr = arr01;
+    $event.currentTarget.style.borderTop = "";
+    const dragIndex = +$event.dataTransfer.getData("dragIndex");
+    arr.splice(index, 1, ...arr.splice(dragIndex, 1, arr[index]));
+  },
 };
 const log = (i) => {
   console.log(i);
