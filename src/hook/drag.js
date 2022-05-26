af47e660ec8fadc47920dc605de31560f71db60e
张圣 <zhangsheng@fineyun.cn>
2022-05-26 09:49:01 +0800

    拖拽指令的描述

--- src/hook/drag.js
+++ src/hook/drag.js
@@ -1,6 +1,12 @@
-//const vDrag =
+/*
+拖拽指令
+值是一个Object
+index属性：v-for接收到的index
+arr属性：v-for遍历的数组
+*/
 export default (el, binding) => {
     const { index, arr } = binding.value;
+    if (!Array.isArray(arr)) throw new Error('arr必须是一个数组')
     el.draggable = true;
     el.ondragstart = (e) => {
         e.dataTransfer.setData("dragIndex", index);
