9b1e7ed8893758dd0a1c596aa5536401bf151273
Swz <xtcff082421@gmail.com>
2022-06-02 22:41:28 +0800

    v-showImg初成

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,7 +1,46 @@
 <template>
-  <div></div>
+  <div v-showImg>
+    <img src="@/assets/logo.png" alt="" />
+  </div>
 </template>
 <script setup>
+const vShowImg = (el) => {
+  let ranStr = [];
+  for (let i = 0; i < 8; i++) {
+    ranStr.push(Math.floor(Math.random() * 16).toString(16));
+  }
+  el.setAttribute(`showImg-${ranStr.join("")}`, "");
+  const imgArr = document.querySelectorAll("[showimg] img");
+  [...imgArr].forEach((img) => {
+    img.draggable = false;
+    /*  img.oncontextmenu = (event) => {
+      event.preventDefault();
+    }; */
+    img.addEventListener("click", (event) => {
+      event.preventDefault();
+      const showImg = document.createElement("img");
+      showImg.draggable = false;
+      showImg.src = event.currentTarget.src;
+      const mask = document.createElement("div");
+      mask.style.position = "fixed";
+      mask.style.top = 0;
+      mask.style.right = 0;
+      mask.style.bottom = 0;
+      mask.style.left = 0;
+      mask.style.zIndex = 999;
+      mask.style.display = "flex";
+      mask.style.justifyContent = "center";
+      mask.style.alignItems - "center";
+      mask.style.backgroundColor = "rgba(0,0,0,.5)";
+      mask.appendChild(showImg);
+      document.body.appendChild(mask);
+      mask.onclick = (event) => {
+        if (event.target === event.currentTarget)
+          document.body.removeChild(mask);
+      };
+    });
+  });
+};
 </script>
 <style lang='scss' scoped>
 </style>
