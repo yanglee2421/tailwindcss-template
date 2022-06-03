0a09a23ae08378c386fc3b4643f8835c0881debe
煎饼果子加十个蛋 <3182703224@qq.com>
2022-06-03 23:28:35 +0800

    装v-showImg集成到getStart插件中

--- src/plugins/getStart.js
+++ src/plugins/getStart.js
@@ -56,6 +56,45 @@ export default {
                 setupState.focus();
             }
         })
+        //v-showImg
+        app.directive('showImg', (el) => {
+            let arr = [];
+            for (let i = 0; i < 8; i++) {
+                arr.push(Math.floor(Math.random() * 16).toString(16));
+            }
+            const ranStr = arr.join("");
+            el.setAttribute(`showImg-${ranStr}`, "");
+            const imgArr = document.querySelectorAll(`[showimg-${ranStr}] img`);
+            [...imgArr].forEach((img) => {
+                img.draggable = false;
+                /*  img.oncontextmenu = (event) => {
+                  event.preventDefault();
+                }; */
+                img.addEventListener("click", (event) => {
+                    event.preventDefault();
+                    const showImg = document.createElement("img");
+                    showImg.draggable = false;
+                    showImg.src = event.currentTarget.src;
+                    const mask = document.createElement("div");
+                    mask.style.position = "fixed";
+                    mask.style.top = 0;
+                    mask.style.right = 0;
+                    mask.style.bottom = 0;
+                    mask.style.left = 0;
+                    mask.style.zIndex = 999;
+                    mask.style.display = "flex";
+                    mask.style.justifyContent = "center";
+                    mask.style.alignItems - "center";
+                    mask.style.backgroundColor = "rgba(0,0,0,.5)";
+                    mask.appendChild(showImg);
+                    document.body.appendChild(mask);
+                    mask.onclick = (event) => {
+                        if (event.target === event.currentTarget)
+                            document.body.removeChild(mask);
+                    };
+                });
+            });
+        })
         console.log(app.version);
     }
 }
