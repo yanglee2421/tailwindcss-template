0425542fdd124e28f89cd9fef1b810484e971422
Swz <xtcff082421@gmail.com>
2022-07-16 16:06:39 +0800

    grid布局实现一个表头固定的表格

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,82 +1,68 @@
 <template>
-  <div class="m-center b">
-    <table cellspacing="0">
-      <thead>
-        <tr>
-          <td class="b-0">定值</td>
-          <td class="b-0"></td>
-          <td class="b-0">
-            <img
-              src="@/assets/alarm.png"
-              style="height:30px"
-            >
-          </td>
-        </tr>
-        <tr>
-          <th>类别</th>
-          <th>温度</th>
-          <th>湿度</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td class="text-nowrap">箱内温度</td>
-          <td>
-            <p class="text-nowrap mb-1">
-              <span>设定值：</span>
-              <span>0.0% ℃</span>
-            </p>
-            <p>
-              <span>0.00% ℃</span>
-            </p>
-          </td>
-          <td>
-            <p class="text-nowrap mb-1">
-              <span>设定值：</span>
-              <span>0.0% RH</span>
-            </p>
-            <p>
-              <span>0.00% RH</span>
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td>HMV</td>
-          <td>0.0%</td>
-          <td>0.0%</td>
-        </tr>
-        <tr>
-          <td>CMV</td>
-          <td>0.0%</td>
-          <td>0.0%</td>
-        </tr>
-      </tbody>
-    </table>
+  <div class="table">
+    <div class="grid m-center thead">
+      <div>111</div>
+      <div>2222</div>
+      <div>3333</div>
+    </div>
+    <div class="grid m-center tbody overflow-overlay">
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam necessitatibus repellat exercitationem, laboriosam quo unde reprehenderit eum minima repellendus non cumque atque ullam quidem, voluptate facilis pariatur. Temporibus, maxime perferendis!</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+      <div>7</div>
+      <div>8</div>
+      <div>9</div>
+      <div>10</div>
+      <div>11</div>
+      <div>12</div>
+      <div>13</div>
+      <div>14</div>
+      <div>15</div>
+      <div>16</div>
+      <div>17</div>
+      <div>18</div>
+      <div>19</div>
+      <div>20</div>
+      <div>21</div>
+    </div>
   </div>
 </template>
+<script lang='ts' setup>
+</script>
 <style lang='scss' scoped>
-table {
-  th {
-    height: 38px;
+.table {
+  ::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+    background-color: transparent;
   }
-  th,
-  td {
-    padding: 10px;
-    border: solid gray;
-    border-width: 1px 1px 0 0;
-    text-align: center;
-    &:first-of-type {
-      border: solid gray;
-      border-width: 1px 1px 0 1px;
+  ::-webkit-scrollbar-thumb {
+    border-radius: 8px;
+    background-color: rgba(0, 0, 0, 0.2);
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.3);
     }
   }
-  tbody tr:last-of-type {
-    td {
-      border-bottom: 1px solid gray;
-    }
+  .thead {
+    height: 100px;
+  }
+  .tbody {
+    height: 300px;
+    border: gray solid;
+    border-width: 0 0 1px 1px;
   }
 }
-.text-nowrap {
-  white-space: nowrap;
+.grid {
+  display: grid;
+  grid-template: auto/repeat(3, 1fr);
+  width: 500px;
+  div {
+    border: gray solid;
+    border-width: 1px 1px 0 0;
+    text-align: center;
+  }
 }
 </style>
