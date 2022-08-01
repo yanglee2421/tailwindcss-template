59666032264af4c0f57d0ab04e3b4e1678891060
张圣 <zhangsheng@fineyun.cn>
2022-08-01 15:09:30 +0800

    插值表达式与&&

--- src/hook/test.js
+++ src/hook/test.js
@@ -1,18 +1,3 @@
-const obj = {
-    [Symbol.asyncIterator]: async function* () {
-        for (let i = 0; i < 3; i++) {
-            yield Promise.resolve(i)
-        }
-    },
-    [Symbol.iterator]: function* () {
-        for (let i = 10; i < 13; i++) {
-            yield Promise.resolve(i)
-        }
-    },
-    [Symbol.toStringTag]: "1111"
-}
-const res = obj[Symbol.asyncIterator]()
-console.log(res.next())
-console.log(res.next())
-console.log(res.next())
-console.log(res.next())
+const test = null && 123
+console.log(test)
+console.log(null && 123)
--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,3 +1,14 @@
-<template></template>
-<script>
+<template>
+  <div>
+    <p>{{state.num&&`${state.num}h`}}</p>
+  </div>
+</template>
+<script lang='ts' setup>
+import { reactive } from "vue";
+
+const state = reactive({
+  num: null,
+});
 </script>
+<style lang='scss' scoped>
+</style>
