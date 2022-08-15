d54867bd76f733b11dddf6699455a98297bb21be
张圣 <zhangsheng@fineyun.cn>
2022-08-15 11:06:14 +0800

    Slot不能同keep-alive一样进行缓存

---
+++ src/components/SlotDemo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>以下是插槽接收到的内容：</h2>
+    <slot
+      :msg="state.msg"
+      show="777"
+    >插槽占位</slot>
+  </div>
+</template>
+<script lang='ts' setup>
+import { reactive } from "vue";
+
+const state = reactive({
+  msg: "要传给插槽内容的数据",
+});
+</script>
+<style lang='scss' scoped>
+</style>
--- src/views/404.vue
+++ src/views/404.vue
@@ -1,16 +1,29 @@
 <template>
   <div>
-    <keep-alive>
+    <!-- <keep-alive>
       <template v-if="state.visible">
         <hello-world></hello-world>
       </template>
-    </keep-alive>
+    </keep-alive> -->
+    <template v-if="state.visible">
+      <slot-demo>
+        <template #default="{msg,show}">
+          <hello-world></hello-world>
+          <ul>
+            <li>1-{{msg}}</li>
+            <li>2-{{show}}</li>
+          </ul>
+        </template>
+      </slot-demo>
+    </template>
     <el-button @click="state.visible=!state.visible">change</el-button>
   </div>
 </template>
 <script lang='ts' setup>
 import { reactive } from "vue";
 import { useRoute, onBeforeRouteLeave, onBeforeRouteUpdate } from "vue-router";
+import SlotDemo from "@/components/SlotDemo.vue";
+import HelloWorld from "@/components/HelloWorld.vue";
 const route = useRoute();
 const state = reactive({
   visible: true,
