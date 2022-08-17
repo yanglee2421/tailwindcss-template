f9553e6543fa6e76e0cbab3062dccc8bfbe117c2
张圣 <zhangsheng@fineyun.cn>
2022-08-17 11:29:22 +0800

    main的滚动

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,7 +16,10 @@
       <el-menu-item index="3">123</el-menu-item>
       <el-menu-item index="4">123</el-menu-item>
     </el-menu>
-    <main class="flex-1-hidden p-1">
+    <main
+      class="flex-1-auto p-1"
+      style="overflow:overlay"
+    >
       <router-view />
     </main>
   </div>
