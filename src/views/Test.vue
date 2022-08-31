6dc5b3cdbaefad7d61896c6c8508418a7e6d0a01
张圣 <zhangsheng@fineyun.cn>
2022-08-31 15:14:06 +0800

    blob转file

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -2,46 +2,12 @@
   <div class="h-100">
     <img
       :src="state.src"
-      id="qrcode"
+      id="qrcodeImg"
     />
-    <!--  <fw-table
-      :model="formData"
-      :data="[{title:'123'},{}]"
-      @fw-query="initTable($event)"
-      v-model:PageIndex="pagination.PageIndex"
-      v-model:PageSize="pagination.PageSize"
-      :total="100"
-      border
-      index
-    >
-      <template #form>
-        1234
-        <template></template>
-        123456
-        <div></div>
-        45646
-        <el-form-item label="跟进日期：">
-          <el-date-picker
-            v-model="formData.data"
-            type="daterange"
-          />
-        </el-form-item>
-        <el-form-item
-          label="通常五个字："
-          v-for="item in 4"
-          :key="item"
-        >
-          <el-input />
-        </el-form-item>
-      </template>
-      <template #tool-bar>
-        <el-button>新增</el-button>
-      </template>
-      <el-table-column
-        label="标题"
-        prop="title"
-      />
-    </fw-table> -->
+    <canvas
+      :src="state.src"
+      id="qrcodeCanvas"
+    ></canvas>
   </div>
 </template>
 <script lang="ts">
@@ -56,9 +22,9 @@ const state = reactive({
   src: "",
 });
 onMounted(() => {
-  console.log(Qrcode);
-  const dom = document.querySelector<HTMLImageElement>("#qrcode")!;
-  const res = Qrcode.toDataURL("汉字", {
+  const img = document.querySelector<HTMLImageElement>("#qrcodeImg")!;
+  const canvas = document.querySelector<HTMLCanvasElement>("#qrcodeCanvas")!;
+  const res = Qrcode.toCanvas(canvas, "汉字", {
     // width: 81,
     margin: 1,
     color: {
@@ -76,37 +42,19 @@ onMounted(() => {
     version: 4,
     errorCorrectionLevel: "H",
     maskPattern: 7,
-    // toSJISFuNC:()=>{}
   });
-  console.log(res);
   res.then((res) => {
-    console.log(res);
-    // dom.innerHTML = res;
-    dom.src = res;
+    console.dir(img);
+    canvas.toBlob((blob) => {
+      console.log(blob);
+      const file = new File([blob] as any, "文件名");
+      console.dir(file);
+    });
   });
-  /* res.then((res) => {
-    dom?.appendChild(res);
-    const url = res.toDataURL();
-    console.log(url);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "图.png";
-    // a.click();
-  });
-  */
 });
-const formData = reactive({
-  input: "",
-  data: [],
-});
-const pagination = reactive({
-  PageIndex: 1,
-  PageSize: 10,
-});
-const initTable = (boolean: boolean) => {
-  boolean && Object.assign(pagination, formData);
-  console.log("发个请求", boolean, pagination);
-};
 </script>
 <style lang='scss' scoped>
+:deep(img + img) {
+  width: 1000px;
+}
 </style>
