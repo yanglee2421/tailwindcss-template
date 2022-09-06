1477c904eca55bce6079f51ccf30d6f022b40314
张圣 <zhangsheng@fineyun.cn>
2022-09-06 13:57:39 +0800

    v-track的注释整理

--- src/hooks/v-track/index.ts
+++ src/hooks/v-track/index.ts
@@ -4,12 +4,10 @@ interface _value {
     times: number,//用户浏览的次数
     eventArr: unknown[],//被点击的元素
 }
-import App from "@/components/HelloWorld.vue"
 interface _binding {
     value: _value
     arg: string
     modifiers: Record<string, boolean>
-    instance?: typeof App
 }
 let begTime = 0
 // 事件控制器
@@ -17,28 +15,31 @@ const controller = new AbortController()
 const signal = controller.signal
 export default {
     mounted(el: HTMLElement, binding: _binding) {
-        console.log(binding)
-        const { value, instance } = binding
-        begTime = Date.now()//本次浏览的开始时间
+        const { value } = binding
+        //获取本次浏览的开始时间
+        begTime = Date.now()
+        // 获取浏览次数
         const localTimes = localStorage.getItem("track-times")
         value.times = 1 + (localTimes ? Number(localTimes) : 0)
         localStorage.setItem("track-times", value.times.toString())
+        // 绑定点击事件
         el.addEventListener("click", (event: Event) => {
             value.count || (value.count = 0)
             value.count++
             Array.isArray(value.eventArr) || (value.eventArr = [])
             value.eventArr?.push((event.target as HTMLElement).innerText)
         })
-        // 用户半闭标签
+        // 绑定close事件
         window.addEventListener("close", () => {
             value.time = Date.now() - begTime
             console.log(value)
         }, { signal })
     },
+    // 组件卸载时
     beforeUnmount(el: HTMLElement, binding: _binding) {
         const { value } = binding
         value.time = Date.now() - begTime
-        console.log(value)
         controller.abort()
+        console.log(value)
     }
 }
