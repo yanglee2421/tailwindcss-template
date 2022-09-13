69b58608bb31905f7f4bf760fb9840fc5d9c388d
张圣 <zhangsheng@fineyun.cn>
2022-09-13 15:01:34 +0800

    全屏弹窗的

--- src/views/Test/Test04.vue
+++ src/views/Test/Test04.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     <fw-switch v-model="state.val"></fw-switch>
+    <el-dialog
+      v-model="state.val"
+      fullscreen
+    >
+      <el-form>
+        <el-form-item label="123">
+          <el-input></el-input>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 <script lang='ts'>
