a051c8a09fffb35e4ac6ab4a9b1cb7edd86992b5
李又土 <xtcff082421@gmail.com>
2022-09-14 19:21:32 +0800

    vscode重配匹

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
-  <router-view #default="{Component}">
+  <router-view #default="{ Component }">
     <component :is="Component" />
   </router-view>
 </template>
 
-<script lang="ts" setup>
-</script>
+<script lang="ts" setup></script>
 
 <style lang="scss">
 @import "@/assets/default.scss";
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from "path"
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   // 路径别名
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   // css预处理器
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: '@import "@/assets/index.scss";'
-      }
-    }
+        additionalData: '@import "@/assets/index.scss";',
+      },
+    },
   },
   // 开发服务器代理
   server: {
@@ -24,10 +24,10 @@ export default defineConfig({
       "/api": {
         target: "http://10.32.16.160:1818",
         // target: "http://192.168.3.9:1818",
-        rewrite: path => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\/api/, ""),
         changeOrigin: true,
         ws: true,
-      }
+      },
     },
   },
-})
+});
