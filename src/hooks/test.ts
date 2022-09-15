abb5e78cac5f02d7cef1cbae51b34af00419ae37
张圣 <zhangsheng@fineyun.cn>
2022-09-15 17:09:03 +0800

    Promise类型只能指定成功的值

--- src/hooks/test.ts
+++
@@ -1,8 +0,0 @@
-/* const fun = (params, str) => {
-  params.forEach((item) => {
-    Array.isArray(item.children) && fun(item.children, str);
-    item.children = item.children.filter((el) => el.label.includes(str));
-  });
-};
-export default fun;
- */
