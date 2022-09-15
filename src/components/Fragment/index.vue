f75ab1e7ce06e0a55a8632cb902862e075481aa0
张圣 <zhangsheng@fineyun.cn>
2022-09-15 10:45:54 +0800

    Fragment组件

---
+++ src/components/Fragment/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <template v-if="true">
+    <slot></slot>
+  </template>
+</template>
+<script lang="ts">
+export default {
+  inheritAttrs: true,
+};
+</script>
+<script lang="ts" setup></script>
+<style lang="scss" scoped></style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,15 @@
-export { default as FwTable } from "./Table/FwTable.vue"
-export { default as SwzAttrs } from "./Attrs/SwzAttrs.vue"
-export { default as SwzTable } from "./Table/SwzTable.vue"
-export { default as SwzSwiper } from "./Swiper/SwzSwiper.vue"
-export { default as SwzSwiperV2 } from "./Swiper/SwzSwiperV2.vue"
-export { default as InputNum } from "./Input/Input-num.vue"
-export { default as SwzVis } from "./Vis/SwzVis.vue"
-export { default as HelloWorld } from "./HelloWorld.vue"
-export { default as SwzCode } from "./Card/SwzCode.vue"
-export { default as SwzDialog } from "./Dialog/SwzDialog.vue"
-export { default as FwSwitch } from "./Switch/FwSwitch.vue"
-export { default as FwCard } from "./Card/FwCard.vue"
+export { default as SwzAttrs } from "./Attrs/SwzAttrs.vue";
+export { default as SwzTable } from "./Table/SwzTable.vue";
+export { default as SwzSwiper } from "./Swiper/SwzSwiper.vue";
+export { default as SwzSwiperV2 } from "./Swiper/SwzSwiperV2.vue";
+export { default as SwzVis } from "./Vis/SwzVis.vue";
+export { default as SwzDialog } from "./Dialog/SwzDialog.vue";
+export { default as SwzCode } from "./Card/SwzCode.vue";
+export { default as SwzFragment } from "./Fragment/index.vue";
+// else
+export { default as HelloWorld } from "./HelloWorld.vue";
+export { default as InputNum } from "./Input/Input-num.vue";
+// Fw
+export { default as FwSwitch } from "./Switch/FwSwitch.vue";
+export { default as FwTable } from "./Table/FwTable.vue";
+export { default as FwCard } from "./Card/FwCard.vue";
