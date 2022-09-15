378214aebe78e33bbdc59e4e200b50ed97c81ba8
张圣 <zhangsheng@fineyun.cn>
2022-09-15 10:53:09 +0800

    swz-fragment

--- src/views/Test/Test04.vue
+++ src/views/Test/Test04.vue
@@ -1,57 +1,57 @@
 <template>
   <div>
     <fw-switch v-model="state.val"></fw-switch>
-    <el-button @click="state.val = '50px'"
-      >50px</el-button
-    >
-    <el-button @click="state.val = '10vh'">
-      10vh
-    </el-button>
-    <el-button @click="state.val = ''"
-      >空串</el-button
-    >
-    <el-button @click="state.val = 0"
-      >0</el-button
-    >
+    <el-button @click="state.val = '50px'">50px</el-button>
+    <el-button @click="state.val = '10vh'"> 10vh </el-button>
+    <el-button @click="state.val = ''">空串</el-button>
+    <el-button @click="state.val = 0">0</el-button>
     <p>
       {{ text }}
     </p>
-    <div v-vis="state.val" class="w-25 m-3">
-      Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Ipsum delectus ipsam nihil
-      perferendis ea quidem similique, maxime
-      iste, laboriosam dicta at. Fuga excepturi
-      quam ex hic voluptatibus provident mollitia
-      tempore?
+    <div
+      v-vis="state.val"
+      class="w-25 m-3"
+    >
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum delectus
+      ipsam nihil perferendis ea quidem similique, maxime iste, laboriosam dicta
+      at. Fuga excepturi quam ex hic voluptatibus provident mollitia tempore?
     </div>
+    <ul>
+      <li>123</li>
+      <li>123</li>
+      <li>123</li>
+      <!-- prettier-ignore -->
+      <swz-fragment>
+        <li key="123" @click="null">456</li>
+        <li key="123" @click="null">456</li>
+        <li key="123" @click="null">456</li>
+      </swz-fragment>
+    </ul>
   </div>
 </template>
 <script lang="ts">
-  export default {
-    inheritAttrs: true,
-  };
+export default {
+  inheritAttrs: true,
+};
 </script>
 <script lang="ts" setup>
-  import { reactive, watchEffect } from "vue";
-  import { useRoute } from "vue-router";
-  const route = useRoute();
-  console.log("query", route.query);
-  console.log("history", history.state);
-  interface _props {
-    text: string;
-  }
-  const props = withDefaults(
-    defineProps<_props>(),
-    {
-      text: "",
-    }
-  );
-  const state = reactive<any>({
-    val: false,
-  });
-  watchEffect(() => {
-    console.log(props.text);
-  });
-  const aa = 123;
+import { reactive, watchEffect } from "vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+console.log("query", route.query);
+console.log("history", history.state);
+interface _props {
+  text: string;
+}
+const props = withDefaults(defineProps<_props>(), {
+  text: "",
+});
+const state = reactive<any>({
+  val: false,
+});
+watchEffect(() => {
+  console.log(props.text);
+});
+const aa = 123;
 </script>
 <style lang="scss" scoped></style>
