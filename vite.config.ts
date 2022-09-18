4c89806b36c9eba07698b57e0b04d8e0b2a7ab94
张圣 <zhangsheng@fineyun.cn>
2022-09-18 12:06:26 +0800

    根据情景决定配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,32 +2,40 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  // 路径别名
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  // css预处理器
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "@/assets/index.scss";',
+export default defineConfig(({ command, mode }) => {
+  const res = {
+    plugins: [vue()],
+    // 路径别名
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
-  // 开发服务器代理
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://10.32.16.160:4399",
-        // target: "http://192.168.3.9:1818",
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        changeOrigin: true,
-        ws: true,
+    // css预处理器
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/index.scss";',
+        },
       },
     },
-  },
+    // 开发服务器代理
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://10.32.16.160:1818",
+          rewrite: (path) => path.replace(/^\/api/, ""),
+          changeOrigin: true,
+          ws: true,
+        },
+      },
+    },
+  };
+  switch (mode) {
+    case "home":
+      res.server.proxy["/api"].target = "http://192.168.3.9:1818";
+      break;
+    default:
+  }
+  return res;
 });
