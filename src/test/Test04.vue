5c9d0afb9aebb226dad1d5fc03b8a09aa1a4beb4
张圣 <zhangsheng@fineyun.cn>
2022-09-20 16:49:59 +0800

    watchPostEffect的坑

--- src/test/Test04.vue
+++ src/test/Test04.vue
@@ -1,15 +1,24 @@
 <template>
-  <div>
-    <swz-dialog>
-      <el-form-item></el-form-item>
-      <el-form-item></el-form-item>
-    </swz-dialog>
-  </div>
+  <div ref="root"></div>
 </template>
 <script lang="ts">
 export default {
   inheritAttrs: true,
 };
 </script>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref, reactive, watchPostEffect, onMounted } from "vue";
+
+const state = reactive<any>({
+  ref: null,
+});
+const root = ref();
+watchPostEffect(() => {
+  // console.log(state.ref);
+  console.log(root.value);
+});
+onMounted(() => {
+  console.log(root.value);
+});
+</script>
 <style lang="scss" scoped></style>
