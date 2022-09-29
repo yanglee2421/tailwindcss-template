c2dd067d27b60bbbdc46af2c0d3627ca88cd746c
张圣 <zhangsheng@fineyun.cn>
2022-09-29 19:11:01 +0800

    ts修复了默认参数关闭类型警的问题

--- src/test/Test.vue
+++ src/test/Test.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="h-100 root">
-    <el-card></el-card>
-    <el-card></el-card>
-    <el-card></el-card>
-    <el-card></el-card>
+    <el-card>
+      <div :ref="(ref) => (chart.lt = ref)"></div>
+    </el-card>
+    <el-card>
+      <div :ref="(ref) => (chart.rt = ref)"></div>
+    </el-card>
+    <el-card>
+      <div :ref="(ref) => (chart.rb = ref)"></div>
+    </el-card>
+    <el-card>
+      <div :ref="(ref) => (chart.lb = ref)"></div>
+    </el-card>
   </div>
 </template>
 <script lang="ts">
@@ -11,7 +19,21 @@ export default {
   inheritAttrs: true,
 };
 </script>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import * as echarts from "echarts";
+import useCharts from "@/hooks/useCharts";
+const a = null;
+useCharts(a as any, [0]);
+const chart: Record<string, unknown> = {
+  rt: null,
+};
+const renderRt = (dom: HTMLElement, data = []) => {
+  const charts = echarts.getInstanceByDom(dom) || echarts.init(dom);
+  const option = {};
+  charts.setOption(option);
+  return charts;
+};
+</script>
 <style lang="scss" scoped>
 .root {
   display: grid;
