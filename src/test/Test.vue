d05cc0dc2dcd0de4f1efb82a1c71ede31c25cc15
李又土 <xtcff082421@gmail.com>
2022-10-01 00:53:44 +0800

    清除浏览器用户代理样式表的scss汇总

--- src/test/Test.vue
+++ src/test/Test.vue
@@ -1,17 +1,6 @@
 <template>
-  <div class="h-100 root">
-    <el-card>
-      <div :ref="(ref) => (chart.lt = ref)"></div>
-    </el-card>
-    <el-card>
-      <div :ref="(ref) => (chart.rt = ref)"></div>
-    </el-card>
-    <el-card>
-      <div :ref="(ref) => (chart.rb = ref)"></div>
-    </el-card>
-    <el-card>
-      <div :ref="(ref) => (chart.lb = ref)"></div>
-    </el-card>
+  <div>
+    <div class="test"></div>
   </div>
 </template>
 <script lang="ts">
@@ -19,26 +8,5 @@ export default {
   inheritAttrs: true,
 };
 </script>
-<script lang="ts" setup>
-import * as echarts from "echarts";
-import useCharts from "@/hooks/useCharts";
-const a = null;
-useCharts(a as any, [0]);
-const chart: Record<string, unknown> = {
-  rt: null,
-};
-const renderRt = (dom: HTMLElement, data: unknown = []) => {
-  const charts = echarts.getInstanceByDom(dom) || echarts.init(dom);
-  const option = {};
-  charts.setOption(option);
-  return charts;
-};
-</script>
-<style lang="scss" scoped>
-.root {
-  display: grid;
-  grid-template: repeat(2, 1fr) / repeat(2, 1fr);
-  gap: 10px;
-  padding: 10px;
-}
-</style>
+<script lang="ts" setup></script>
+<style lang="scss" scoped></style>
