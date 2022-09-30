462967d138ebd27411125a2b13aea43f05b43479
张圣 <zhangsheng@fineyun.cn>
2022-09-30 14:28:47 +0800

    dateFormat中参数为10发生的bug

--- src/tool/else/dateFormat.ts
+++ src/tool/else/dateFormat.ts
@@ -1,16 +1,21 @@
-const dateFormat = (date: Date | number | string = Date.now(), needSecond = true) => {
-    const fun = (num: number) => {
-        return num > 10 ? num : `0${num}`
-    }
-    if (typeof date !== 'object') {
-        date = new Date(date)
-    }
-    const yyyy = date.getFullYear()
-    let MM = fun(date.getMonth() + 1)
-    let dd = fun(date.getDate())
-    let hh = fun(date.getHours())
-    let mm = fun(date.getMinutes())
-    let ss = fun(date.getSeconds())
-    return needSecond ? `${yyyy}-${MM}-${dd} ${hh}:${mm}:${ss}` : `${yyyy}-${MM}-${dd}`
-}
-export default dateFormat
+const dateFormat = (
+  date: Date | number | string = Date.now(),
+  needSecond = true
+) => {
+  const fun = (num: number) => {
+    return num > 9 ? num : `0${num}`;
+  };
+  if (typeof date !== "object") {
+    date = new Date(date);
+  }
+  const yyyy = date.getFullYear();
+  let MM = fun(date.getMonth() + 1);
+  let dd = fun(date.getDate());
+  let hh = fun(date.getHours());
+  let mm = fun(date.getMinutes());
+  let ss = fun(date.getSeconds());
+  return needSecond
+    ? `${yyyy}-${MM}-${dd} ${hh}:${mm}:${ss}`
+    : `${yyyy}-${MM}-${dd}`;
+};
+export default dateFormat;
