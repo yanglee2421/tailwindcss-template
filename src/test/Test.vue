522e2e21dd3a90e4cb2e188e5e795d9c80e5e85a
张圣 <zhangsheng@fineyun.cn>
2022-10-08 15:56:40 +0800

    升级依赖

--- src/test/Test.vue
+++ src/test/Test.vue
@@ -4,9 +4,43 @@
   </div>
 </template>
 <script lang="ts">
+import { ref, watch } from "vue";
+
 export default {
   inheritAttrs: true,
+  data() {
+    return {
+      r: 0,
+    };
+  },
+  created() {
+    console.log("created");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  setup() {
+    const r = ref(0);
+    watch(
+      r,
+      () => {
+        console.log("watch-setup");
+      },
+      { immediate: true }
+    );
+    console.log("setup");
+  },
+  watch: {
+    r: {
+      immediate: true,
+      handler() {
+        console.log("watch");
+      },
+    },
+  },
 };
 </script>
-<script lang="ts" setup></script>
 <style lang="scss" scoped></style>
