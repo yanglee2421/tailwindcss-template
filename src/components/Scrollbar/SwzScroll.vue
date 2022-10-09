00a6664c8f2c50ff569527427408c76196d2ab4d
张圣 <zhangsheng@fineyun.cn>
2022-10-09 18:50:32 +0800

    传统方式的滚动条

--- src/components/Scrollbar/SwzScroll.vue
+++ src/components/Scrollbar/SwzScroll.vue
@@ -5,9 +5,9 @@
   >
     <aside class="thumb-x"></aside>
     <aside class="thumb-y"></aside>
-    <div class="client">
+    <section class="client">
       <slot></slot>
-    </div>
+    </section>
   </div>
 </template>
 <script lang="ts">
@@ -19,9 +19,65 @@ export default {
 import type { Directive } from "vue";
 const vScroll: Directive<HTMLElement> = {
   mounted(dom) {
-    const { clientWidth, clientHeight } = dom;
+    const { top, left } = dom.getBoundingClientRect();
+    const client = dom.querySelector<HTMLElement>(":scope>.client")!;
+    const { clientWidth, clientHeight, scrollWidth, scrollHeight } = client;
+    /**
+     * 横向滚动条
+     */
     const thumbX = dom.querySelector<HTMLElement>(":scope>.thumb-x")!;
+    thumbX.style.width = `${Math.floor(clientWidth ** 2 / scrollWidth)}px`;
+    thumbX.addEventListener("mousedown", (event) => {
+      thumbX.classList.add("thumb-focus");
+      const { offsetX } = event;
+      const controller = new AbortController();
+      const signal = controller.signal;
+      document.addEventListener(
+        "mousemove",
+        (event) => {
+          event.preventDefault();
+          const { clientX } = event;
+          const x = clientX - offsetX - left;
+          const maxX = clientWidth - thumbX.offsetWidth;
+          const translate = x < 0 ? 0 : x > maxX ? maxX : x;
+          thumbX.style.transform = `translateX(${translate}px)`;
+          client.scrollLeft = (translate * scrollWidth) / clientWidth;
+        },
+        { signal }
+      );
+      document.addEventListener("mouseup", () => {
+        controller.abort();
+        thumbX.classList.remove("thumb-focus");
+      });
+    });
+    /**
+     * 纵向滚动条
+     */
     const thumbY = dom.querySelector<HTMLElement>(":scope>.thumb-y")!;
+    thumbY.style.height = `${Math.floor(clientHeight ** 2 / scrollHeight)}px`;
+    thumbY.addEventListener("mousedown", (event) => {
+      thumbY.classList.add("thumb-focus");
+      const { offsetY } = event;
+      const controller = new AbortController();
+      const signal = controller.signal;
+      document.addEventListener(
+        "mousemove",
+        (event) => {
+          event.preventDefault();
+          const { clientY } = event;
+          const y = clientY - offsetY - top;
+          const maxY = clientHeight - thumbY.offsetHeight;
+          const translate = y < 0 ? 0 : y > maxY ? maxY : y;
+          thumbY.style.transform = `translateY(${translate}px)`;
+          client.scrollTop = (translate * scrollHeight) / clientHeight;
+        },
+        { signal }
+      );
+      document.addEventListener("mouseup", () => {
+        controller.abort();
+        thumbY.classList.remove("thumb-focus");
+      });
+    });
   },
 };
 </script>
@@ -46,6 +102,9 @@ const vScroll: Directive<HTMLElement> = {
     background-color: rgba(0, 0, 0, 0.3);
   }
 }
+.thumb-focus {
+  background-color: rgba(0, 0, 0, 0.4) !important;
+}
 .thumb-x {
   @include thumb;
   @include position(auto, auto, 0, 0);
@@ -56,7 +115,6 @@ const vScroll: Directive<HTMLElement> = {
   @include thumb;
   @include position(0, 0, auto, auto);
   width: 8px;
-  height: 30px;
 }
 .client {
   height: 100%;
