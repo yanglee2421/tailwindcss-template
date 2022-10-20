9ca3c019843f5f6de17cfa13d827ed05e4e3a95e
李又土 <xtcff082421@gmail.com>
2022-10-20 21:48:34 +0800

    删除不必要的文件，图标替换

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
-import type { UserConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  const config: UserConfig = {
+  return {
     plugins: [vue()],
     resolve: {
       alias: {
@@ -33,5 +32,4 @@ export default defineConfig(({ command, mode }) => {
       assetsDir: "assets",
     }, */
   };
-  return config;
 });
