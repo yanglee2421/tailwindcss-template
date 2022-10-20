dc4e5c063ac9a3e6ed374e244c7ab53f8b2dbfc3
张圣 <zhangsheng@fineyun.cn>
2022-10-20 14:12:51 +0800

    同域下才能以a标签的download属性触发预览图片的下载

--- src/test/Test.vue
+++ src/test/Test.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <div class="test"></div>
+    <div class="test">
+      <a
+        href="http://127.0.0.1/pic-blob"
+        target="_blank"
+        >图片预览</a
+      >
+      <br />
+      <img
+        src="http://127.0.0.1/bing"
+        alt=""
+      />
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -8,5 +19,15 @@ export default {
   inheritAttrs: true,
 };
 </script>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const downloadFn = () => {
+  const a = document.createElement("a");
+  a.href = "http://127.0.0.1/public/bg.jpg";
+  a.target = "_blank";
+  // 需要同域才能触发下载
+  a.download = "pic.jpg";
+  a.click();
+  a.remove();
+};
+</script>
 <style lang="scss" scoped></style>
