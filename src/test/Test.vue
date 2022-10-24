df7a74354d81b817e01d8535fd62c21acb447685
李又土 <xtcff082421@gmail.com>
2022-10-24 21:06:04 +0800

    element-plus的折叠面板性能很强

--- src/test/Test.vue
+++ src/test/Test.vue
@@ -1,5 +1,44 @@
 <template>
-  <div></div>
+  <div>
+    <el-collapse>
+      <el-collapse-item
+        name="1"
+        label="表单"
+      >
+        <el-form>
+          <el-form-item
+            v-for="(item, index) in 6"
+            :key="index"
+          >
+            <el-input></el-input>
+          </el-form-item>
+        </el-form>
+      </el-collapse-item>
+    </el-collapse>
+    <el-table
+      :data="[{}, {}, {}, {}, {}, {}, {}]"
+      border
+      stripe
+    >
+      <el-table-column
+        label="序号"
+        type="index"
+        align="center"
+        width="55px"
+      ></el-table-column>
+      <el-table-column
+        type="selection"
+        align="center"
+      ></el-table-column>
+      <el-table-column label="标题"></el-table-column>
+      <el-table-column label="标题"></el-table-column>
+      <el-table-column label="标题"></el-table-column>
+      <el-table-column label="标题"></el-table-column>
+      <el-table-column label="标题"></el-table-column>
+      <el-table-column label="标题"></el-table-column>
+      <el-table-column label="标题"></el-table-column>
+    </el-table>
+  </div>
 </template>
 <script lang="ts">
 export default {
