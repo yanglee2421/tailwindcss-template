333569cf32a84e41f33e406d556ef32cf158daf9
张圣 <zhangsheng@fineyun.cn>
2022-10-31 11:29:06 +0800

    引入简历

--- src/test/Test.vue
+++ src/test/Test.vue
@@ -1,10 +1,19 @@
 <template>
-  <div></div>
+  <div class="h-100 overflow-hidden">
+    <embed
+      :src="pdf"
+      type="application/pdf"
+      width="100%"
+      height="100%"
+    />
+  </div>
 </template>
 <script lang="ts">
 export default {
   inheritAttrs: true,
 };
 </script>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import pdf from "@/assets/resume.pdf";
+</script>
 <style lang="scss" scoped></style>
