eca63c67e4199644ca1252698cf9c0fde1849df2
李又土 <xtcff082421@gmail.com>
2022-12-11 03:44:30 +0800

    hash+嵌入式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import routes from "./routes";
 import { usePinia } from "@/hook";
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 // 网页标题
