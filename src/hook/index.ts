b00c8fc6dc30d09910f5bae8b689e69155c13633
李又土 <xtcff082421@gmail.com>
2022-12-11 04:45:47 +0800

    hook与util的区别

--- src/hook/index.ts
+++ src/hook/index.ts
@@ -1,6 +1,3 @@
 export { default as useClientWidth } from "./useClientWidth";
-export { default as useDateFormat } from "./useDateFormat";
-export { default as useDownloadFile } from "./useDownloadFile";
 export { default as usePermission } from "./usePermission";
 export { default as usePinia } from "./usePinia";
-export { default as useDark } from "./useDark";
