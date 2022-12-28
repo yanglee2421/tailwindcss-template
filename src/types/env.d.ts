b87c1f627f5d4544b6e08b48e75b3bb532b2d5c3
煎饼果子加十个蛋 <3182703224@qq.com>
2022-12-29 01:32:47 +0800

    升级依赖

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -5,7 +5,7 @@
  */
 /// <reference types="vite/client" />
 declare module "*.vue" {
-  import type { DefineComponent, HtmlHTMLAttributes } from "vue";
+  import type { DefineComponent } from "vue";
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
