9a07602ffae3b5c83e3c94c45a8c02a6566078b4
煎饼果子加十个蛋 <3182703224@qq.com>
2023-01-05 18:34:21 +0800

    发到gitee试水

--- vite.config.ts
+++ vite.config.ts
@@ -2,38 +2,38 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  return {
-    plugins: [vue()],
-    // 路径别名
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command, mode }) => ({
+  plugins: [vue()],
+  // 路径别名
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  // CSS预处理器
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@use "@/assets/index.scss" as *;',
       },
     },
-    // CSS预处理器
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: '@use "@/assets/index.scss" as *;',
-        },
+  },
+  // 开发服务器
+  server: {
+    port: 5174,
+    proxy: {
+      "/dev": {
+        target: "http://192.168.1.4",
+        rewrite: (path) => path.replace(/^\/dev/, ""),
+        changeOrigin: true,
+        ws: true,
       },
     },
-    // 开发服务器
-    server: {
-      port: 5174,
-      proxy: {
-        "/dev": {
-          target: "http://192.168.1.4",
-          rewrite: (path) => path.replace(/^\/dev/, ""),
-          changeOrigin: true,
-          ws: true,
-        },
-      },
-    },
-    base: "/vue/",
-    build: {
-      outDir: "vue-app",
-    },
-  };
-});
+  },
+  // 路由
+  base: mode === "gitee" ? "./" : "/vue/",
+  // 构建
+  build: {
+    outDir: mode === "gitee" ? "dist" : "vue-app",
+  },
+}));
