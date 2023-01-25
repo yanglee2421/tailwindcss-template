0d3ddb258a84f1776304847b023e89594f4635ca
煎饼果子加十个蛋 <3182703224@qq.com>
2023-01-25 13:52:14 +0800

    useResize改良

--- src/hook/useResize.ts
+++ src/hook/useResize.ts
@@ -1,20 +1,31 @@
 import { onBeforeUnmount, onMounted, ref, unref } from "vue";
+namespace Type {
+  interface cbParams {
+    width: number;
+    height: number;
+  }
+  export interface cb {
+    (params: cbParams): void | Function;
+  }
+}
 /**
  * 监听 dom resize 的钩子
  * @param callback 挂载、dom resize时执行
  * @returns 用于指定 dom 的 ref
  */
-export function useResize(
-  callback: (params: { width: number; height: number }) => void
-) {
+export function useResize(callback: Type.cb) {
   const resizeRef = ref();
+  let clearFn: void | Function;
   const observer = new ResizeObserver(
     ([
       {
         contentBoxSize: [{ inlineSize: width, blockSize: height }],
       },
     ]) => {
-      callback({ width, height });
+      if (typeof clearFn === "function") {
+        clearFn();
+      }
+      clearFn = callback({ width, height });
     }
   );
   onMounted(() => {
