4672f98d4f89199193d0eb44ada1b541c1690ae4
煎饼果子加十个蛋 <3182703224@qq.com>
2023-02-02 23:13:41 +0800

    纯gzip不行

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import imagemin from "vite-plugin-imagemin";
 
 // https://vitejs.dev/config/
 export default defineConfig((ConfigEnv) => ({
-  plugins: [vue(), gzip({ deleteOriginFile: true }), imagemin()],
+  plugins: [vue(), gzip({ deleteOriginFile: false }), imagemin()],
   resolve: {
     alias: { "@": resolve(__dirname, "./src") },
   },
