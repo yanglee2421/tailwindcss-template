a2ecf4ec178ce10cbe041d7ce544026b463cfbeb
Yang_Lee <yanglee2421@outlook.com>
2023-03-24 22:08:45 +0800

    reset.scss

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import getStart from "@/plugin";
-// import "@/a"
+import "@/assets/reset.scss";
 
 const app = createApp(App);
 app.use(getStart);
