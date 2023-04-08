355f42bcc1b4e3e7a14ab44759377173387d0888
Yang_Lee <yanglee2421@outlook.com>
2023-04-08 21:30:39 +0800

    use tinymce cdn replace bunding

--- src/component/tinymce/tinymce-editor.vue
+++ src/component/tinymce/tinymce-editor.vue
@@ -1,54 +1,15 @@
 <script lang="ts" setup>
 import Editor from "@tinymce/tinymce-vue";
-import tinymce from "tinymce/tinymce";
-import "tinymce/models/dom/model";
-import "tinymce/themes/silver";
-import "tinymce/icons/default";
-import "tinymce/skins/ui/oxide/skin.min.css";
-
-import "tinymce/plugins/advlist";
-import "tinymce/plugins/anchor";
-import "tinymce/plugins/autolink";
-import "tinymce/plugins/autoresize";
-import "tinymce/plugins/autosave";
-import "tinymce/plugins/charmap";
-import "tinymce/plugins/code";
-import "tinymce/plugins/codesample";
-import "tinymce/plugins/directionality";
-import "tinymce/plugins/emoticons";
-import "tinymce/plugins/fullscreen";
-import "tinymce/plugins/help";
-import "tinymce/plugins/image";
-import "tinymce/plugins/importcss";
-import "tinymce/plugins/insertdatetime";
-import "tinymce/plugins/link";
-import "tinymce/plugins/lists";
-import "tinymce/plugins/media";
-import "tinymce/plugins/nonbreaking";
-import "tinymce/plugins/pagebreak";
-import "tinymce/plugins/preview";
-import "tinymce/plugins/quickbars";
-import "tinymce/plugins/save";
-import "tinymce/plugins/searchreplace";
-import "tinymce/plugins/table";
-import "tinymce/plugins/template";
-import "tinymce/plugins/visualblocks";
-import "tinymce/plugins/visualchars";
-import "tinymce/plugins/wordcount";
-
-import "tinymce/plugins/emoticons/js/emojis";
-
-console.log(tinymce);
 
 function init() {
   return {
-    height: 500,
-    branding: false,
-    skin: false,
+    skin: true,
+    content_css: true,
     menubar: false,
+    branding: false,
+    height: 500,
     plugins: plugins(),
     toolbar: toolbar(),
-    content_css: false,
     content_style: content_style(),
   };
 }
