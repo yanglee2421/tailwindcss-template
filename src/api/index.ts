8cd89815a760c102e281bda7a341748c4210cec4
zhangsheng <yanglee2421@gmail.com>
2023-04-26 23:45:28 +0800

    plan

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,9 @@
 export async function getDemo<TData>(data: TData) {
+  console.log("request", data);
+  await timeout();
   return data;
+}
+
+async function timeout(milliseconds = 1000) {
+  await new Promise((res) => setTimeout(res, milliseconds));
 }
--- src/page/demo/page-demo.vue
+++ src/page/demo/page-demo.vue
@@ -1,60 +1,114 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { getDemo } from "@/api";
+import { useQueries, UseQueryOptions } from "@tanstack/vue-query";
+import { computed, reactive, unref } from "vue";
+
+interface Item {
+  id: number;
+  status: string;
+}
+
+const state = reactive({
+  qeueTasks: [] as Item[],
+  settledTasks: [] as any[],
+  role_no: 1,
+});
+const qeueTasks = computed(() =>
+  state.qeueTasks.filter((item, idx) => {
+    return idx % state.role_no === 0;
+  })
+);
+type QueryOptions = UseQueryOptions<Item, Error, Item, unknown[]>;
+const queries = computed(() => {
+  return unref(qeueTasks).map<QueryOptions>((item) => ({
+    queryKey: ["task", item],
+    queryFn: () => getDemo(item),
+    staleTime: Infinity,
+    retry: 3,
+    retryDelay: 1000 * 3,
+    initialData: { id: item.id, status: "pending" },
+    initialDataUpdatedAt: 0,
+    onSuccess(data) {
+      console.log("success");
+
+      const idx = state.qeueTasks.indexOf(item);
+      state.qeueTasks.splice(idx, 1);
+      state.settledTasks.unshift(data);
+    },
+    onError(err) {},
+    onSettled() {},
+  }));
+});
+
+const queryList = useQueries({ queries });
+const tableData = computed(() => {
+  return queryList.slice().concat(state.settledTasks);
+});
+
+const handleAdd = () => {
+  state.qeueTasks.unshift({
+    id: Date.now(),
+    status: "pending",
+  });
+};
+</script>
 
 <template>
-  <div class="box"></div>
+  <div class="flex-column p-1">
+    <div>
+      <el-radio-group v-model="state.role_no">
+        <el-radio :label="1">label1</el-radio>
+        <el-radio :label="2">label2</el-radio>
+        <el-radio :label="3">label3</el-radio>
+      </el-radio-group>
+    </div>
+    <div class="p-1">
+      <el-button
+        @click="handleAdd"
+        type="primary"
+        >Add</el-button
+      >
+    </div>
+    <div class="flex-1-hidden">
+      <el-table
+        :data="tableData"
+        height="100%"
+        border
+      >
+        <el-table-column label="id">
+          <template #default="{ row }">
+            {{ row.data ? row.data.id : row.id }}
+          </template>
+        </el-table-column>
+        <el-table-column label="status">
+          <template #default="{ row }">
+            {{ row.data ? row.data.status : "success" }}
+          </template>
+        </el-table-column>
+        <el-table-column label="operate">
+          <template #default="{ row }">
+            <el-button
+              v-if="row.data"
+              @click="row.refetch"
+              type="primary"
+              link
+              >refetch</el-button
+            >
+            <el-button
+              v-else
+              type="danger"
+              link
+            >
+              view
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped>
-:global(.dark) {
-  .header {
-    @include acrylic(true);
-  }
-}
-.box {
-  height: 100vh;
-  // background-image: url("@/assets/image/bg/bg.jpg");
-  @include bgc-img;
-}
-.header {
-  @include acrylic;
-  @extend .shadow, .px-2;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  color: #555;
-  font-family: "consolas";
-  font-size: 1rem;
-  font-weight: 600;
-}
-.header-content {
-  flex: 1;
-  display: flex;
-  align-items: center;
-  justify-content: end;
-}
-.icon-search {
-  display: flex;
-  align-items: center;
-  // justify-content: start;
-  padding: 1rem;
-
-  @media (min-width: 575px) {
-    flex: 1;
-  }
-}
-.ly-icon {
-  display: none !important;
-}
-
-@media (max-width: 575px) {
-  .nav-menu {
-    display: none;
-  }
-  .ly-icon {
-    display: block !important;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
 <script lang="ts">
 export default { inheritAttrs: true };
 </script>
