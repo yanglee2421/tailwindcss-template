fd0b88a98fdb7b30314f81b6f93ffeb085968cf6
ZhangSheng <yanglee2421@gmail.com>
2023-06-07 16:14:22 +0800

    grid

--- src/page/demo/page-demo.vue
+++ src/page/demo/page-demo.vue
@@ -1,57 +1,24 @@
-<script lang="ts" setup>
-import { cardRight } from "./components";
-import { provide, reactive } from "vue";
-import type { GlobalState } from "@/types/pageDemo";
-
-const globalState = reactive<GlobalState>({
-  pendingTask: [],
-  settledTasks: [],
-});
-provide("global-state", globalState);
-
-const form = reactive({
-  role_no: 1,
-});
-
-const handleAdd = () => {
-  globalState.pendingTask.unshift({
-    client_task_id: crypto.randomUUID(),
-    role_no: 1,
-    task_id: "",
-    task_label: "new",
-    task_progress: 0,
-    task_status: "PENDING",
-    start_time: new Date().toISOString(),
-    end_time: null,
-    exception: null,
-    form: {},
-  });
-};
-</script>
+<script lang="ts" setup></script>
 
 <template>
-  <div class="flex-column p-1">
-    <div>
-      <el-radio-group v-model="form.role_no">
-        <el-radio :label="1">label1</el-radio>
-        <el-radio :label="2">label2</el-radio>
-        <el-radio :label="3">label3</el-radio>
-      </el-radio-group>
-    </div>
-    <div class="p-1">
-      <el-button
-        @click="handleAdd"
-        type="primary"
-        >Add</el-button
-      >
-    </div>
-    <div class="flex-1-hidden">
-      <card-right></card-right>
-    </div>
+  <div class="page">
+    <div
+      v-for="item in 5"
+      :key="item"
+      class="b"
+    ></div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.page {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+  grid-auto-rows: 10rem;
+
+  height: 10rem;
+}
+</style>
 <script lang="ts">
 export default { inheritAttrs: true };
 </script>
