191a76a6911789760658f3fdf7b2033264c73156
ZhangSheng <yanglee2421@gmail.com>
2023-08-21 14:55:58 +0800

    sort out code

--- src/page/demo/components/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as cardRight } from "./card-right.vue";
--- src/router/router-whitelist.ts
+++ src/router/router-whitelist.ts
@@ -1,4 +1,4 @@
-export const whiteList: string[] = ["404", "absen"];
+export const whiteList: string[] = ["login"];
 
 export const toIsWhitelist = (id: string) => {
   return whiteList.includes(id);
