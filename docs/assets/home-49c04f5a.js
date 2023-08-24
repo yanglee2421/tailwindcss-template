35e3817009bb41e4156a9d574d753ba099a11c56
Yang_Lee <yanglee2421@outlook.com>
2023-08-25 00:10:11 +0800

    page home

--- docs/assets/home-49c04f5a.js
+++
@@ -1,1 +0,0 @@
-import{d as e,o as t,e as o}from"./vue-a77f30ea.js";const c=e({inheritAttrs:!1,__name:"home",setup(n){return(r,a)=>(t(),o("div"))}});export{c as default};
--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -1,9 +1,75 @@
 <script lang="ts" setup>
+import viteLogo from "@/assets/image/logo/react.svg";
+
 defineOptions({ inheritAttrs: false });
 </script>
 
 <template>
-  <div></div>
+  <div class="page">
+    <el-row :gutter="16">
+      <el-col
+        :xs="24"
+        :sm="12"
+      >
+        <el-text tag="h1">Wellcome to JavaScript!</el-text>
+        <el-text>JavaScript</el-text>
+      </el-col>
+      <el-col
+        :xs="24"
+        :sm="12"
+      >
+        <div class="logo--bg">
+          <el-image :src="viteLogo"></el-image>
+        </div>
+      </el-col>
+      <el-col
+        v-for="item in 6"
+        :key="item"
+        :xs="24"
+        :sm="12"
+        :md="8"
+      >
+        <el-card>{{ item }}</el-card>
+      </el-col>
+    </el-row>
+    <el-card></el-card>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.page {
+  max-width: 1440px;
+  padding: 1rem;
+  margin: 0 auto;
+}
+.el-col {
+  margin-bottom: 1rem;
+}
+h1.el-text {
+  font-size: 3.5rem;
+  word-break: normal;
+}
+.logo--bg {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  $size: 20rem;
+  width: $size;
+  height: $size;
+
+  &::before {
+    content: "";
+    position: absolute;
+    inset: 0;
+    // z-index: 1;
+    display: block;
+    width: calc($size / 1.2);
+    height: calc($size / 1.2);
+    border-radius: 50%;
+    margin: auto;
+    background-color: $danger;
+    filter: blur($size * 0.1);
+  }
+}
+</style>
