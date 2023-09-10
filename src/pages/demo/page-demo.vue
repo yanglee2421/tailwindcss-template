f36da7d4400c3f46a96984e4d0cd787b6398d5f6
Yang_Lee <yanglee2421@outlook.com>
2023-09-11 00:07:41 +0800

    layout demo page

--- src/pages/demo/page-demo.vue
+++ src/pages/demo/page-demo.vue
@@ -1,71 +1,68 @@
 <script lang="ts" setup>
 // Vue Imports
-import { onMounted, ref, unref } from "vue";
+import { ref, unref } from "vue";
 
 const boxRef = ref<HTMLDivElement>();
 
-onMounted(() => {
+const handleSort = () => {
   const container = unref(boxRef);
   if (!container) return;
 
-  const sort = document.getElementById("sort")!;
-  sort.onclick = () => {
-    record(container);
-    change();
-    play(container);
-  };
+  record(container);
+  change(container);
+  play(container);
+};
 
-  function record(container: Element | null) {
-    if (!container) return;
+function record(container: Element | null) {
+  if (!container) return;
 
-    for (let i = 0, len = container.children.length; i < len; i++) {
-      const dom = container.children[i];
-      const rect = dom.getBoundingClientRect();
-      Reflect.set(dom, "startX", rect.left);
-      Reflect.set(dom, "startY", rect.top);
+  for (let i = 0, len = container.children.length; i < len; i++) {
+    const dom = container.children[i];
+    const rect = dom.getBoundingClientRect();
+    Reflect.set(dom, "startX", rect.left);
+    Reflect.set(dom, "startY", rect.top);
+  }
+}
+
+function change(container: Element | null) {
+  if (!container) return;
+
+  const childrens = [...container.children];
+  for (let i = 0, l = childrens.length; i < l; i++) {
+    const children = childrens[i];
+    const j = Math.floor(Math.random() * l);
+    if (i !== j) {
+      // 获取当前dom的下一个元素
+      const inextDom = children.nextElementSibling;
+      // 把i插入j之前
+      container.insertBefore(children, childrens[j]);
+      // 把下标j的元素插入到i元素之前
+      container.insertBefore(childrens[j], inextDom);
     }
   }
+}
 
-  function change() {
-    if (!container) return;
+function play(container: Element | null) {
+  if (!container) return;
 
-    const childrens = [...container.children];
-    for (let i = 0, l = childrens.length; i < l; i++) {
-      const children = childrens[i];
-      const j = Math.floor(Math.random() * l);
-      if (i !== j) {
-        // 获取当前dom的下一个元素
-        const inextDom = children.nextElementSibling;
-        // 把i插入j之前
-        container.insertBefore(children, childrens[j]);
-        // 把下标j的元素插入到i元素之前
-        container.insertBefore(childrens[j], inextDom);
-      }
-    }
+  for (let i = 0, len = container.children.length; i < len; i++) {
+    const dom = container.children[i];
+    const { left, top } = dom.getBoundingClientRect();
+
+    const startX = Reflect.get(dom, "startX");
+    const startY = Reflect.get(dom, "startY");
+
+    dom.animate(
+      [
+        {
+          transform: `translate(${startX - left}px, ${startY - top}px)`,
+        },
+        { transform: `translate(0px, 0px)` },
+      ],
+      { duration: 600 }
+    );
   }
-
-  function play(container: Element | null) {
-    if (!container) return;
-
-    for (let i = 0, len = container.children.length; i < len; i++) {
-      const dom = container.children[i];
-      const { left, top } = dom.getBoundingClientRect();
-
-      const startX = Reflect.get(dom, "startX");
-      const startY = Reflect.get(dom, "startY");
-
-      dom.animate(
-        [
-          {
-            transform: `translate(${startX - left}px, ${startY - top}px)`,
-          },
-          { transform: `translate(0px, 0px)` },
-        ],
-        { duration: 600 }
-      );
-    }
-  }
-});
+}
 
 defineOptions({ inheritAttrs: false });
 </script>
@@ -73,6 +70,7 @@ defineOptions({ inheritAttrs: false });
 <template>
   <div class="text-center">
     <el-button
+      @click="handleSort"
       size="large"
       type="primary"
       class="bg-sky-500 uppercase"
@@ -82,7 +80,7 @@ defineOptions({ inheritAttrs: false });
   </div>
   <div
     ref="boxRef"
-    class="grid grid-cols-12 gap-2 p-2 overflow-hidden mx-auto mt-6 border"
+    class="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 xl:grid-cols-12 gap-2 p-2 overflow-hidden mx-auto mt-6 border"
   >
     <div
       v-for="item in 50"
