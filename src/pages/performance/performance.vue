2e5cd62f47a33dd9be0d8e553d808d72b44b9fa4
ZhangSheng <yanglee2421@gmail.com>
2023-09-26 14:37:01 +0800

    sticky top 0

--- src/pages/performance/performance.vue
+++ src/pages/performance/performance.vue
@@ -3,13 +3,11 @@ defineOptions({ inheritAttrs: false });
 </script>
 
 <template>
-  <div class="h-full overflow-auto flex flex-col">
-    <header class="pb-6 sticky top-0 bg-white shadow">
+  <div class="h-full flex flex-col">
+    <header class="pb-6 bg-white shadow fixed top-0 inset-x-0">
       <h1 class="text-7xl mx-auto w-fit">position sticky</h1>
     </header>
-    <div
-      class="mx-auto mt-6 max-w-screen-lg grid grid-cols-2 gap-6 flex-1 overflow-auto"
-    >
+    <div class="mx-auto mt-28 max-w-screen-lg grid grid-cols-2 gap-6">
       <ul class="space-y-2">
         <li
           v-for="item in 10"
@@ -19,7 +17,7 @@ defineOptions({ inheritAttrs: false });
           {{ item }}
         </li>
       </ul>
-      <article class="border sticky top-0 self-start">
+      <article class="border sticky top-28 self-start p-3">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem minima
         temporibus consequatur porro. Consequatur eligendi dicta veniam
         repudiandae sit dignissimos error esse perferendis illo iure!
