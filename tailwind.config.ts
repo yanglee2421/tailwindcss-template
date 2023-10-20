1ef5ba5ae37078082a3aacaedc3f831144a5e95b
ZhangSheng <yanglee2421@gmail.com>
2023-10-20 09:17:44 +0800

    Config tailwindcss by ts

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+// Tailwind Imports
+import { Config } from "tailwindcss";
+
+export default defineConfig();
+
+function defineConfig(): Config {
+  return {
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    theme: {
+      extend: {},
+      screens: {
+        sm: "768px",
+        md: "992px",
+        lg: "1200px",
+        xl: "1920px",
+        "2xl": "1920px",
+      },
+    },
+    plugins: [],
+    corePlugins: {
+      preflight: true,
+    },
+    important: "#root",
+  };
+}
