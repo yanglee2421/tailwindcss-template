8442c04ec234cff8c383abb390234a326e394e49
ZhangSheng <yanglee2421@gmail.com>
2023-10-24 14:53:26 +0800

    sort out code

--- src/pages/demo/page-demo.vue
+++ src/pages/demo/page-demo.vue
@@ -1,95 +1,9 @@
 <script lang="ts" setup>
-// Vue Imports
-import { ref, unref } from "vue";
-
-const boxRef = ref<HTMLDivElement>();
-
-const handleSort = () => {
-  const container = unref(boxRef);
-  if (!container) return;
-
-  record(container);
-  change(container);
-  play(container);
-};
-
-function record(container: Element | null) {
-  if (!container) return;
-
-  for (let i = 0, len = container.children.length; i < len; i++) {
-    const dom = container.children[i];
-    const rect = dom.getBoundingClientRect();
-    Reflect.set(dom, "startX", rect.left);
-    Reflect.set(dom, "startY", rect.top);
-  }
-}
-
-function change(container: Element | null) {
-  if (!container) return;
-
-  const childrens = [...container.children];
-  for (let i = 0, l = childrens.length; i < l; i++) {
-    const children = childrens[i];
-    const j = Math.floor(Math.random() * l);
-    if (i !== j) {
-      // 获取当前dom的下一个元素
-      const inextDom = children.nextElementSibling;
-      // 把i插入j之前
-      container.insertBefore(children, childrens[j]);
-      // 把下标j的元素插入到i元素之前
-      container.insertBefore(childrens[j], inextDom);
-    }
-  }
-}
-
-function play(container: Element | null) {
-  if (!container) return;
-
-  for (let i = 0, len = container.children.length; i < len; i++) {
-    const dom = container.children[i];
-    const { left, top } = dom.getBoundingClientRect();
-
-    const startX = Reflect.get(dom, "startX");
-    const startY = Reflect.get(dom, "startY");
-
-    dom.animate(
-      [
-        {
-          transform: `translate(${startX - left}px, ${startY - top}px)`,
-        },
-        { transform: `translate(0px, 0px)` },
-      ],
-      { duration: 600 }
-    );
-  }
-}
-
 defineOptions({ inheritAttrs: false });
 </script>
 
 <template>
-  <div class="text-center">
-    <el-button
-      @click="handleSort"
-      size="large"
-      type="primary"
-      class="bg-sky-500 uppercase"
-      id="sort"
-      >random order</el-button
-    >
-  </div>
-  <div
-    ref="boxRef"
-    class="p-2 overflow-hidden mx-auto mt-6 border flex gap-2 flex-wrap"
-  >
-    <div
-      v-for="item in 50"
-      :key="item"
-      class="basis-1/6 sm:basis-1/12 flex-1 bg-red-300 flex justify-center items-center h-16 border rounded text-white text-2xl"
-    >
-      {{ item }}
-    </div>
-  </div>
+  <div></div>
 </template>
 
 <style lang="scss" scoped></style>
--- src/routers/router-routes.ts
+++ src/routers/router-routes.ts
@@ -4,86 +4,106 @@ export const routes: RouterOptions["routes"] = [
   { path: "/:pathMatch(.*)*", redirect: "/404" },
   {
     path: "/404",
-    component: () => import("@/pages/404/404.vue"),
     name: "404",
     meta: { title: "NotFound" },
+    component() {
+      return import("@/pages/404/404.vue");
+    },
   },
   {
     path: "/login",
-    component: () => import("@/pages/login/login.vue"),
     name: "login",
     meta: { title: "登录" },
+    component() {
+      return import("@/pages/login/login.vue");
+    },
   },
   {
     path: "/",
-    component: () => import("@/pages/home/home.vue"),
-    children: [
-      {
-        path: "",
-        meta: { title: "首页" },
-        name: "home",
-        component() {
-          return import("@/pages/demo/page-demo.vue");
-        },
-      },
-      {
-        path: "/particle",
-        component: () => import("@/pages/particle/particle.vue"),
-        name: "particle",
-        meta: { title: "粒子动画" },
-      },
-      {
-        path: "/snow",
-        component: () => import("@/pages/snow/snow.vue"),
-        name: "snow",
-        meta: { title: "雪飘" },
-      },
-      {
-        path: "/shapes",
-        component: () => import("@/pages/shapes/page-shapes.vue"),
-        name: "shapes",
-        meta: { title: "shapes" },
-      },
-      {
-        path: "/scroll",
-        component() {
-          return import("@/pages/scroll");
-        },
-        name: "scroll",
-        meta: { title: "scroll" },
-      },
-      {
-        path: "performance",
-        name: "performance",
-        meta: { title: "Performance" },
-        component() {
-          return import("@/pages/performance");
-        },
-      },
-      {
-        path: "cropperjs",
-        name: "cropperjs",
-        meta: { title: "cropperjs" },
-        component() {
-          return import("@/pages/cropperjs");
-        },
-      },
-      {
-        path: "grid",
-        name: "grid",
-        meta: { title: "grid" },
-        component() {
-          return import("@/pages/grid");
-        },
-      },
-      {
-        path: "table",
-        name: "table",
-        meta: { title: "table" },
-        component() {
-          return import("@/pages/table");
-        },
-      },
-    ],
+    name: "home",
+    meta: { title: "Home" },
+    component() {
+      return import("@/pages/home/home.vue");
+    },
+  },
+  {
+    path: "/demo",
+    name: "demo",
+    meta: { title: "demo" },
+    component() {
+      return import("@/pages/demo/page-demo.vue");
+    },
+  },
+  {
+    path: "/particle",
+    name: "particle",
+    meta: { title: "粒子动画" },
+    component() {
+      return import("@/pages/particle/particle.vue");
+    },
+  },
+  {
+    path: "/snow",
+    name: "snow",
+    meta: { title: "雪飘" },
+    component() {
+      return import("@/pages/snow/snow.vue");
+    },
+  },
+  {
+    path: "/shapes",
+    name: "shapes",
+    meta: { title: "shapes" },
+    component() {
+      return import("@/pages/shapes/page-shapes.vue");
+    },
+  },
+  {
+    path: "/scroll",
+    name: "scroll",
+    meta: { title: "scroll" },
+    component() {
+      return import("@/pages/scroll");
+    },
+  },
+  {
+    path: "/performance",
+    name: "performance",
+    meta: { title: "Performance" },
+    component() {
+      return import("@/pages/performance");
+    },
+  },
+  {
+    path: "/cropperjs",
+    name: "cropperjs",
+    meta: { title: "cropperjs" },
+    component() {
+      return import("@/pages/cropperjs");
+    },
+  },
+  {
+    path: "/grid",
+    name: "grid",
+    meta: { title: "grid" },
+    component() {
+      return import("@/pages/grid");
+    },
+  },
+  {
+    path: "/table",
+    name: "table",
+    meta: { title: "table" },
+    component() {
+      return import("@/pages/table");
+    },
+  },
+  {
+    path: "/flip",
+    meta: { title: "flip" },
+    name: "flip",
+    component() {
+      return import("@/pages/flip");
+    },
   },
 ];
