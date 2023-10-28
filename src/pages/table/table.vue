96ae5b79a04f720549c92a318000454f16d14f50
ZhangSheng <yanglee2421@gmail.com>
2023-10-28 11:22:22 +0800

    table layout

--- src/pages/table/table.vue
+++ src/pages/table/table.vue
@@ -1,20 +1,47 @@
 <script lang="ts" setup>
+// Vue Imports
+import { reactive } from "vue";
+
+const pagiStat = reactive({
+  page: 1,
+  pageSize: 20,
+});
+
 defineOptions({ inheritAttrs: false });
 </script>
 
 <template>
-  <el-table :data="[]">
-    <el-table-column
-      label="col 1"
-      width="150"
-    ></el-table-column>
-    <el-table-column label="col 2"></el-table-column>
-    <el-table-column
-      label="col 3"
-      width="150"
-    ></el-table-column>
-    <el-table-column label="col 4"></el-table-column>
-  </el-table>
+  <div class="h-full p-2 flex flex-col">
+    <el-form inline>
+      <el-form-item label="name">
+        <el-input></el-input>
+      </el-form-item>
+    </el-form>
+    <div class="border flex-1 overflow-hidden">
+      <el-table
+        :data="[]"
+        height="100%"
+      >
+        <el-table-column
+          label="col 1"
+          width="150"
+        ></el-table-column>
+        <el-table-column label="col 2"></el-table-column>
+        <el-table-column
+          label="col 3"
+          width="150"
+        ></el-table-column>
+        <el-table-column label="col 4"></el-table-column>
+      </el-table>
+    </div>
+    <el-pagination
+      v-model:current-page="pagiStat.page"
+      v-model:page-size="pagiStat.pageSize"
+      :total="100"
+      :page-sizes="[20, 50, 100]"
+      layout="total,prev,pager,next,jumper"
+    ></el-pagination>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
