594332f07c1bf260663a315f0912caa46c9d5960
ZhangSheng <yanglee2421@gmail.com>
2023-12-06 18:06:27 +0800

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 // Vite Imports
-import { defineConfig, UserConfig } from "vite";
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // NodeJs Imports
@@ -33,70 +33,55 @@ export default defineConfig((ConfigEnv) => {
 
     base: isBuild ? "./" : "/vue-ele",
 
-    // ** Build
-    build: build(),
-
-    // DEV Server
-    server: server(),
-
     // Env file directory
     envDir: resolve(__dirname, "./"),
+
+    // ** Build
+    build: {
+      outDir: "docs",
+      emptyOutDir: true,
+
+      manifest: false,
+      chunkSizeWarningLimit: 1024,
+
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "./index.html"),
+        },
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules/cropperjs")) {
+              return "cropperjs";
+            }
+
+            if (id.includes("node_modules/fabric")) {
+              return "fabric";
+            }
+          },
+          entryFileNames: "assets/[name]-[hash].js",
+          assetFileNames: "assets/[name]-[hash][extname]",
+          chunkFileNames: "assets/[name]-[hash].js",
+        },
+      },
+    },
+    // DEV Server
+    server: {
+      port: 3007,
+      strictPort: true,
+      hmr: {
+        port: 3007,
+      },
+      proxy: {
+        "/dev": {
+          target: "http://localhost:5173",
+          rewrite(path) {
+            return path.replace(/^\/dev/, "");
+          },
+          changeOrigin: true,
+          ws: true,
+        },
+      },
+      fs: { allow: [resolve(__dirname, "./")] },
+    },
   };
 });
-
-// Build Config
-function build(): UserConfig["build"] {
-  const __dirname = dirname(fileURLToPath(import.meta.url));
-
-  return {
-    outDir: "docs",
-    emptyOutDir: true,
-
-    manifest: false,
-    chunkSizeWarningLimit: 1024,
-
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "./index.html"),
-      },
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules/cropperjs")) {
-            return "cropperjs";
-          }
-
-          if (id.includes("node_modules/fabric")) {
-            return "fabric";
-          }
-        },
-        entryFileNames: "assets/[name]-[hash].js",
-        assetFileNames: "assets/[name]-[hash][extname]",
-        chunkFileNames: "assets/[name]-[hash].js",
-      },
-    },
-  };
-}
-
-// Server Config
-function server(): UserConfig["server"] {
-  const __dirname = dirname(fileURLToPath(import.meta.url));
-
-  return {
-    fs: { allow: [resolve(__dirname, "./")] },
-    port: 3007,
-    strictPort: true,
-    hmr: {
-      port: 3007,
-    },
-    proxy: {
-      "/dev": {
-        target: "http://localhost:5173",
-        rewrite(path) {
-          return path.replace(/^\/dev/, "");
-        },
-        changeOrigin: true,
-        ws: true,
-      },
-    },
-  };
-}
