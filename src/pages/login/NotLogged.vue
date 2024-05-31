a7a7f0133a7ae6efa08b4363655dd67a19a851ce
yanglee2421 <yanglee2421@gmail.com>
2024-05-31 13:36:01 +0800

    feat: show password

--- src/pages/login/NotLogged.vue
+++ src/pages/login/NotLogged.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { useForm } from "@tanstack/vue-form";
 import { zodValidator } from "@tanstack/zod-form-adapter";
+import * as Vue from "vue";
 import { z } from "zod";
 import GuestGuard from "@/components/guard/GuestGuard.vue";
 
@@ -14,17 +15,21 @@ const form = useForm({
     console.log(props.value);
   },
 });
+
+const state = Vue.reactive({
+  showPassword: false,
+});
 </script>
 
 <template>
   <GuestGuard>
     <div class="fixed inset-0 flex h-full">
       <!-- Empty Container -->
-      <div class="hidden md:block"></div>
+      <div class="hidden flex-grow md:block"></div>
 
       <!-- Login Form -->
       <div
-        class="ms-auto flex w-full flex-col justify-center border-l p-8 md:max-w-md md:p-6"
+        class="flex w-full flex-col justify-center border-l p-8 shadow md:max-w-md md:p-6"
       >
         <form
           @submit="
@@ -34,11 +39,7 @@ const form = useForm({
               form.handleSubmit();
             }
           "
-          @reset="
-            () => {
-              form.reset();
-            }
-          "
+          @reset="form.reset()"
           novalidate
           autocomplete="off"
           class="space-y-4"
@@ -57,7 +58,12 @@ const form = useForm({
                 >
                 <input
                   type="email"
-                  class="mt-1 block w-full rounded-md border border-slate-300 bg-white px-3 py-2 placeholder-slate-400 shadow-sm invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500 disabled:shadow-none sm:text-sm"
+                  :class="[
+                    'mt-1 block w-full rounded-md border  bg-white px-3 py-2 placeholder-slate-400 shadow-sm focus:outline-none focus:ring-1   disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500 disabled:shadow-none sm:text-sm',
+                    state.meta.errors.length
+                      ? 'border-pink-500 text-pink-600 focus:border-pink-500 focus:ring-pink-500'
+                      : 'border-slate-300 focus:border-sky-500 focus:ring-sky-500',
+                  ]"
                   placeholder="you@example.com"
                   :id="field.name"
                   :name="field.name"
@@ -85,11 +91,22 @@ const form = useForm({
               class="block text-sm font-medium text-slate-700"
               >Password</label
             >
-            <input
-              type="password"
-              class="mt-1 block w-full rounded-md border border-slate-300 bg-white px-3 py-2 placeholder-slate-400 shadow-sm invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500 disabled:shadow-none sm:text-sm"
-              value="Bosco"
-            />
+            <div
+              class="mt-1 flex w-full overflow-hidden rounded-md border border-slate-300 bg-white shadow-sm focus-within:border-sky-500 focus-within:ring-1 focus-within:ring-sky-500"
+            >
+              <input
+                :type="state.showPassword ? 'text' : 'password'"
+                class="flex-grow px-3 py-2 placeholder-slate-400 invalid:border-pink-500 invalid:text-pink-600 focus:outline-none focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500 disabled:shadow-none sm:text-sm"
+                value="Bosco"
+              />
+              <button
+                @click="state.showPassword = !state.showPassword"
+                type="button"
+                class="px-2 hover:bg-slate-50 focus:bg-slate-100 focus:outline-none active:bg-slate-200"
+              >
+                {{ state.showPassword ? "eye" : "0y0" }}
+              </button>
+            </div>
           </div>
           <div>
             <a
@@ -105,6 +122,16 @@ const form = useForm({
             >
               login
             </button>
+          </div>
+          <div
+            class="flex items-center justify-center gap-3 text-sm font-medium"
+          >
+            <span class="text-slate-500">New on out platform?</span>
+            <a
+              href="#"
+              class="text-sky-500 underline"
+              >Create an account</a
+            >
           </div>
           <div class="flex items-center gap-3">
             <hr class="flex-grow border-t" />
