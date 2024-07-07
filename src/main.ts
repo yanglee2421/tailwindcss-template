31913014714b4b656bd773a061c974e3cd49a27b
yanglee2421 <yanglee2421@gmail.com>
2024-07-07 22:51:51 +0800

    style: remove plugins dir

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,46 @@
+import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
+import { persistQueryClient } from "@tanstack/query-persist-client-core";
+import { VueQueryPlugin } from "@tanstack/vue-query";
 import { createPinia } from "pinia";
 import * as Vue from "vue";
 import "@/assets/css/style.css";
 import "@/assets/scss/global.scss";
-import { VueQuery } from "@/plugins/VueQuery";
 import { router } from "@/router/router";
 import App from "./App.vue";
 
 const app = Vue.createApp(App);
 
-app.use(router);
-app.use(createPinia());
-app.use(VueQuery);
+app
+  .use(createPinia())
+  .use(router)
+  .use((app) => {
+    app.use(VueQueryPlugin, {
+      queryClientConfig: {
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60,
+            gcTime: 1000 * 60 * 2,
 
-app.mount("#root");
+            refetchOnMount: true,
+            refetchOnWindowFocus: true,
+            refetchOnReconnect: true,
+
+            retry: 1,
+            retryDelay(attemptIndex) {
+              return Math.min(1000 * 2 ** attemptIndex, 1000 * 8);
+            },
+          },
+        },
+      },
+      clientPersister(queryClient) {
+        return persistQueryClient({
+          queryClient,
+          persister: createAsyncStoragePersister({
+            storage: sessionStorage,
+            key: import.meta.env.VITE_QUERY_PERSISTER_KEY,
+          }),
+        });
+      },
+    });
+  })
+  .mount("#root");
