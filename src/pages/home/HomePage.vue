3efb2fa10949103c36bd2cafc54ca749cf53b8f1
yanglee2421 <yanglee2421@gmail.com>
2024-07-20 12:11:10 +0800

    feat: dialog with animate

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -4,11 +4,11 @@ import * as Vue from "vue";
 import { app } from "@/api/firebase/app";
 import AuthGuard from "@/components/guard/AuthGuard.vue";
 import ScrollView from "@/components/ui/ScrollView.vue";
+import TransitionDialog from "./TransitaionDialog.vue";
 
-const state = Vue.reactive<{
-  showAside: boolean;
-}>({
+const state = Vue.reactive({
   showAside: false,
+  showDialog: false,
 });
 </script>
 
@@ -24,8 +24,8 @@ const state = Vue.reactive<{
         <div class="flex justify-between px-5 py-2">
           <h2 class="text-xl font-medium">aside</h2>
           <button
-            @click="state.showAside = !state.showAside"
             class="flex sm:hidden"
+            @click="state.showAside = !state.showAside"
           >
             list
           </button>
@@ -47,21 +47,40 @@ const state = Vue.reactive<{
           <span class="text-xl uppercase"> header </span>
 
           <button
-            @click="state.showAside = !state.showAside"
             class="flex sm:hidden"
+            @click="state.showAside = !state.showAside"
           >
             list
           </button>
         </header>
         <main class="p-5">
-          <button
-            class="rounded bg-red-500 px-3 py-1.5 text-white hover:bg-red-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500/50 active:bg-red-700 disabled:cursor-not-allowed disabled:bg-red-500/50"
-            @click="signOut(getAuth(app))"
-          >
-            sign out
-          </button>
+          <div class="flex gap-3">
+            <button
+              class="rounded bg-red-500 px-3 py-1.5 text-white hover:bg-red-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500/50 active:bg-red-700 disabled:cursor-not-allowed disabled:bg-red-500/50"
+              @click="signOut(getAuth(app))"
+            >
+              sign out
+            </button>
+            <button
+              class="rounded bg-indigo-500 px-3 py-1.5 font-medium uppercase text-white hover:bg-indigo-600 focus-visible:bg-indigo-700"
+              @click="state.showDialog = !state.showDialog"
+            >
+              open
+            </button>
+          </div>
+          <TransitionDialog :open="state.showDialog">
+            <div class="mt-3 rounded border px-5 py-2">
+              <p>hello dialog</p>
+              <button
+                class="rounded bg-indigo-500 px-3 py-1.5 font-medium uppercase text-white hover:bg-indigo-600 focus-visible:bg-indigo-700"
+                @click="state.showDialog = !state.showDialog"
+              >
+                close
+              </button>
+            </div>
+          </TransitionDialog>
           <div
-            v-for="item in 10"
+            v-for="item in 5"
             :key="item"
             class="h-96"
           ></div>
